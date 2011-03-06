a130f4ee2d30f2de5494f137a93bd8a0b1321fff
Mark Rada <mrada@marketcircle.com>
2011-03-06 13:03:28 -0500

    Make minitest/pride work on MacRuby
    MacRuby doesn't have Fibers right now, so the current
    implementation of minitest/pride does not work.
    
    Simple modulo arithmetic is the substitution.

--- lib/minitest/pride.rb
+++ lib/minitest/pride.rb
@@ -8,18 +8,22 @@ class PrideIO
 
   # stolen from /System/Library/Perl/5.10.0/Term/ANSIColor.pm
   COLORS = (31..36).to_a
+  COLORS_SIZE = COLORS.size
   CHARS = ["*"]
+  CHARS_SIZE = CHARS.size
 
   def initialize io
     @io = io
-    @colors = COLORS.cycle
-    @chars  = CHARS.cycle
+    @colors = COLORS
+    @chars  = CHARS
+    @index  = 0
   end
 
   def print o
     case o
     when "." then
-      io.print "\e[#{@colors.next}m#{@chars.next}\e[0m"
+      @index += 1
+      io.print "\e[#{@colors[@index % COLORS_SIZE]}m#{@chars[@index % CHARS_SIZE]}\e[0m"
     when "E", "F" then
       io.print "\e[41m\e[37m#{o}\e[0m"
     else
