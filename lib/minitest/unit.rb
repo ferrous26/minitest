8ae61ed38d1159e7b84b9824d672b3744d63cf75
Mark Rada <mrada@marketcircle.com>
2011-03-23 17:51:11 -0400

    Bump version to 2.1.2

--- lib/minitest/unit.rb
+++ lib/minitest/unit.rb
@@ -515,7 +515,7 @@ module MiniTest
   end
 
   class Unit
-    VERSION = "2.1.1" # :nodoc:
+    VERSION = "2.1.2" # :nodoc:
 
     attr_accessor :report, :failures, :errors, :skips # :nodoc:
     attr_accessor :test_count, :assertion_count       # :nodoc:
